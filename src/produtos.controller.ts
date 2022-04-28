6eaf716e60959bc440994db8bc763592a0c463bf
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-04-27 21:23:38 -0300

    Criando o ProdutosService

--- src/produtos.controller.ts
+++ src/produtos.controller.ts
@@ -1,37 +1,36 @@
 import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
 import { Produto } from './produto.model';
+import { ProdutosService } from "./produtos.service";
 @Controller('produtos')
 export class ProdutosController {
-    produtos: Produto[] = [
-        new Produto("LIV01", "TDD e BDD na prática", 29.90),
-        new Produto("LIV02", "Iniciando com Flutter", 39.90),
-        new Produto("LIV03", "Inteligência artificial como serviço", 29.90)
-    ];
+    constructor(private produtosService: ProdutosService) {
+
+    }
 
     @Get()
     obterTodos(): Produto[] {
-        return this.produtos;
+        return this.produtosService.obterTodos();
     }
 
     @Get(':id')
     obterUm(@Param() params): Produto {
-        return this.produtos[0];
+        return this.produtosService.obterUm(params.id);
     }
 
     @Post()
     criar(@Body() produto: Produto) {
         produto.id - 100;
 
-        this.produtos.push(produto);
+        this.produtosService.criar(produto);
     }
 
     @Put()
     alterar(@Body() produto: Produto): Produto {
-        return produto;
+        return this.produtosService.alterar(produto);
     }
 
     @Delete(':id')
     apagar(@Param() params) {
-        this.produtos.pop();
+        this.produtosService.apagar(params.id);
     }
 }
